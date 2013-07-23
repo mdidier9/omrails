63d278973f42ebe6ce90cd59b5d91ce6fb249238
Matthew Didier <mdidier9@yahoo.com>
2013-07-23 12:08:37 -0500

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+   
+  # MRD added to eliminate signup error 
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '9bfe9fb72bfe89583f96c39f1d8f493aca1647864278f36a0fa8d861e14a3f66d54510b6ea48b8502e18450289742e8b841b602efb4904bbfb3a22db5ede975d'
